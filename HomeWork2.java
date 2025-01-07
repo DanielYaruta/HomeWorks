df2ccb07462b524c630f7afc00aa8de1912f0333
DanielYaruta <154588574+DanielYaruta@users.noreply.github.com>
2025-01-07 17:57:05 +0300

    Update HomeWork2.java

--- HomeWork2.java
+++ HomeWork2.java
@@ -12,9 +12,9 @@ public class HomeWork2 {
         add(3);
         add(1);
         add(5);
-        print(); // должно вывести 4,3,1,5
+        print();
         sort();
-        print(); // должно вывести 1,3,4,5
+        print();
     }
 
     static void sort() {
